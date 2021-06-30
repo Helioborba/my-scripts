54ac1d29bab5042269f9b08516ac938ce9c1a922
Borba <gabriel.404e@gmail.com>
2021-06-30 16:06:32 -0300

    bug fixing (gitrapido/enmigrate)

--- Bash/.new_commands.sh
+++ Bash/.new_commands.sh
@@ -27,37 +27,56 @@ function enflask() { #Script principal, vai rodar o flask em modo de desenvolvim
 	fi
 }
 
-function enflask() { #Script principal, vai rodar o flask em modo de desenvolvimento, necessita do parametro start
-	checar # execultar a validação do VENV
-	if [ "$1" = 'start' ];
+## Flask migrate/update
+function enmigrate() { #Rapido upgrade (commit) da DB, necessita do parametro da mensagem para ser o commit
+	if [ "$1" = '' ]; # Bloquear caso a pessoa não coloque a mensagem
 	then
-		echo "Iniciando o flask... apenas um segundo!"
-		export FLASK_APP=app
-		export FLASK_ENV=development
-		flask run
-		echo 'flask fechado.'
+		echo "Não foi digitado mensagem!" 
 	else
-		echo "Opção de inicialização não encontrado! tente a opção 'start'!"
+		if [ -d "./migrations" ]; # Checar se já foi criado a db
+		then
+			# Take action if $DIR exists. #
+			echo "Migrations já existe, execultando task..."
+		else
+			echo "Migrations não existe, criando.. "
+			flask db init
+		fi
+		if (( $# > 1 )); # ter certeza que não foi digitado sem aspas a mensagem
+		then
+			echo "Este commando NÃO aceita parametros extras! utilize 'aspas' para a mensagem!"
+		else
+			echo "Realizando operações da DB"
+			flask db migrate -m "$1"
+			flask db upgrade
+			echo "task terminado."
+		fi
 	fi
 }
 
 ###### GIT #######
 ## Commit Simplificado
 function gitrapido() { #Fazer commit com tudo, só necessita da mensagem
-	if (( $# > 1 )); # ter certeza que não foi digitado sem aspas a mensagem
+	if [ "$1" = '' ]; # Bloquear caso a pessoa não coloque a mensagem
 	then
-		echo "Este commando NÃO aceita parametros extras! utilize 'aspas' para a mensagem!"
+		echo "Não foi digitado a mensagem de commit!" 
 	else
-		echo "Execultado o commit"
-		git add *
-		git commit -m "$1"
-		git push
+		if (( $# > 1 )); # ter certeza que não foi digitado sem aspas a mensagem
+		then
+			echo "Este commando NÃO aceita parametros extras! utilize 'aspas' para a mensagem!"
+		else
+			echo "Execultado o commit"
+			git add *
+			git commit -m "$1"
+			git push
+		fi
 	fi
 }
 
-## Criar repo local/Commit para o remote
+
+## Criar repo local/Commit para o remote, é necessario passar o remote como parametro!
 function gitcriar() { # fazer o primeiro push utilizando um diretório que já tenha arquivos
-	read  -n 1 -p "Deseja realmente criar o git neste diretório? (s/n)" selecionado
+	read  -n 1 -p "Por favor, aperte ENTER..." # ter certeza que a pessoa esta lendo o promp
+	read  -n 1 -p "A origin é esta? "$1" (s/n)" selecionado
 	if [ "${selecionado,}" = "s" ];
 	then
 		echo "Criando..."
@@ -65,13 +84,15 @@ function gitcriar() { # fazer o primeiro push utilizando um diretório que já t
 		git add *
 		git commit -m "initial commit" # Não é necessario mudar esta tag
 		git branch -M main
-		git remote add origin https://github.com/Helioborba/my-scripts.git
+		git remote add origin "$1"
 		git push -u origin main
-		echo "Sucesso! task terminado"
+		echo "Sucesso! task terminad."
 	else
 		echo "Cancelado"
 	fi	
 }
 
-
+# function teste12() { # Utilizado como template para alguns commandos
+	
+# }
 
